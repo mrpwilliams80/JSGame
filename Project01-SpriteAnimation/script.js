5af7487f159528e50b2f5e0656761b22d35a27a4
P. Williams <mrpwilliams80@gmail.com>
2022-07-29 13:02:38 +0100

    feat (Project01): edit script.js
    Project01-0.5.0, repostion and resize Image object

--- Project01-SpriteAnimation/script.js
+++ Project01-SpriteAnimation/script.js
@@ -5,7 +5,11 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 //#region - Simple Animation
-// A block moves horizontally from one edge of the canvas and off the other
+/**
+ * Draw a block in the canvas and move it horizontally from one edge till it is 
+ * off the other
+ */
+
 
 // let x = 0;      // starting position
 
@@ -25,15 +29,40 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 
 //#region - Draw Image to Canvas No Modifications
-// draw image from external asset to canvas at position 0,0 without resize
+/**
+ * Draw an image from external asset to canvas, starting at position 0,0 without 
+ * resizing the source
+ */
+
+// const SPRITE_SHEET = new Image();                       // create Image obj
+// SPRITE_SHEET.src = "./Assets/shadow_dog.png";           // set Image src
+
+// function drawSprite() {
+//     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);   // clear canvas
+//     ctx.drawImage(SPRITE_SHEET, 0, 0);                  // draw sprite sheet
+
+//     requestAnimationFrame(drawSprite);              // call func for nxt frame
+// }
+
+// drawSprite();                                           // start animation
+
+//#endregion
+
+
+//#region - Draw Image to Canvas Modify Size & Position
+/**
+ * Draw image from external asset to canvas, starting at position 50, 50 and 
+ * resized to fit inside a margin of 50
+ */
 
 const SPRITE_SHEET = new Image();                       // create Image obj
 SPRITE_SHEET.src = "./Assets/shadow_dog.png";           // set Image src
 
 function drawSprite() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);   // clear canvas
-    ctx.drawImage(SPRITE_SHEET, 0, 0);                  // draw sprite sheet
-
+    
+    // draw sprite sheet
+    ctx.drawImage(SPRITE_SHEET, 50, 50, CANVAS_WIDTH-100, CANVAS_HEIGHT-100);
     requestAnimationFrame(drawSprite);              // call func for nxt frame
 }
 
