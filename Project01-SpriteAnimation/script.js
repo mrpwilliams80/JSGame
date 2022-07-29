af2bf003bd1471838f327f67cf813046cf14eead
P. Williams <mrpwilliams80@gmail.com>
2022-07-29 10:59:49 +0100

    feat (Project01): add script.js
    Project01-0.3.0, basic animation
    NOTE: too fast (1000+fps) on linux, migrating to windows

---
+++ Project01-SpriteAnimation/script.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const SPRITE_SHEET = new Image();
+SPRITE_SHEET.src = "../Assets/shadow_dog.png"
+
+let x = 0;
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillStyle = "#888";
+    ctx.fillRect(x, 50, 100, 100);
+
+    x++;       
+     
+    requestAnimationFrame(animate())
+}
+
+animate();
